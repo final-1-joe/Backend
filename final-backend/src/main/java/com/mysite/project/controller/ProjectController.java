0c201fb5fa4034d8e66aa2e22de504a1174f9b7f
KimeSeongMan <wis06051@gmail.com>
2023-05-29 17:29:06 +0900

    프로젝트 목록 필터기능

--- final-backend/src/main/java/com/mysite/project/controller/ProjectController.java
+++ final-backend/src/main/java/com/mysite/project/controller/ProjectController.java
@@ -2,16 +2,16 @@ package com.mysite.project.controller;
 
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.mysite.project.service.ProjectService;
 import com.mysite.project.vo.ProjectVO;
 
-@RestController
+@Controller
 public class ProjectController {
 	
 	private ProjectService projectService;
