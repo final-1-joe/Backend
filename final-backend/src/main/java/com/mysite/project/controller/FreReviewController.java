5896a65ebb37ba7fb919e4106031b78f6b9baac8
yujinchoi <chldbwls8001@gmail.com>
2023-06-02 02:49:44 +0900

    프로젝트/프리랜서 리뷰 기능 구현 완료

--- final-backend/src/main/java/com/mysite/project/controller/FreReviewController.java
+++ final-backend/src/main/java/com/mysite/project/controller/FreReviewController.java
@@ -3,8 +3,10 @@ package com.mysite.project.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mysite.project.service.FreReviewService;
@@ -21,29 +23,43 @@ public class FreReviewController {
 	
 	@RequestMapping(value = "/frereview/list", method = RequestMethod.GET)
 	@ResponseBody
-	public List<FreReviewVO> getReviewList() {
-		List<FreReviewVO> reviewList = reviewService.getReviewList();
+	public List<FreReviewVO> getReviewList(@RequestParam("fre_rv_target") String fre_rv_target) {
+		List<FreReviewVO> reviewList = reviewService.getReviewList(fre_rv_target);
 		return reviewList;
 	}
 
-	@RequestMapping(value = "/frereview/insert", method = RequestMethod.POST)
+	@RequestMapping(value = "/frereview/write", method = RequestMethod.POST)
 	@ResponseBody
-	public int insertReview(FreReviewVO review) {
+	public int insertReview(@RequestBody FreReviewVO review) {
 		int res = reviewService.insertReview(review);
 		return res;
 	}
 
 	@RequestMapping(value = "/frereview/update", method = RequestMethod.POST)
 	@ResponseBody
-	public int updateReview(FreReviewVO review) {
+	public int updateReview(@RequestBody FreReviewVO review) {
 		int res = reviewService.updateReview(review);
 		return res;
 	}
 
 	@RequestMapping(value = "/frereview/delete", method = RequestMethod.GET)
 	@ResponseBody
-	public int deleteReview(FreReviewVO review) {
-		int res = reviewService.deleteReview(review);
+	public int deleteReview(@RequestParam("fre_rv_num") int fre_rv_num) {
+		int res = reviewService.deleteReview(fre_rv_num);
+		return res;
+	}
+	
+	@RequestMapping(value = "/frereview/cnt", method = RequestMethod.GET)
+	@ResponseBody
+	public int getCount(FreReviewVO review) {
+		int res = reviewService.getCount(review);
+		return res;
+	}
+
+	@RequestMapping(value = "/frereview/tot", method = RequestMethod.GET)
+	@ResponseBody
+	public int getTotalScore(FreReviewVO review) {
+		int res = reviewService.getTotalScore(review);
 		return res;
 	}
 }
