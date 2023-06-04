09474155a58b698c45a2a9b77dd25459a61e2b20
yujinchoi <chldbwls8001@gmail.com>
2023-06-05 05:17:59 +0900

    관심 등록, 지원하기 구현

--- final-backend/src/main/java/com/mysite/project/impl/MarkPjServiceImpl.java
+++ final-backend/src/main/java/com/mysite/project/impl/MarkPjServiceImpl.java
@@ -23,5 +23,11 @@ public class MarkPjServiceImpl implements MarkPjService {
 		List<MyMarkPjVO> markPjList = mapper.selectMarkPj(user_id);
 		return markPjList;
 	}
+	
+	@Override
+	public int registerMarkPj(MarkPjVO vo) {
+		int res = mapper.registerMarkPj(vo);
+		return res;
+	}
 
 }
--- final-backend/src/main/java/com/mysite/project/mapper/MarkFreeMapper.java
+++ final-backend/src/main/java/com/mysite/project/mapper/MarkFreeMapper.java
@@ -2,6 +2,7 @@ package com.mysite.project.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -16,5 +17,8 @@ public interface MarkFreeMapper {
 			+ "ON m.mark_fre_id = r.user_id WHERE m.user_id = #{user_id}")
 	List<MyMarkFreeVO> selectMarkFree(@Param("user_id") String user_id);
 	//클라이언트 마이페이지에서 북마크한 프리랜서 목록을 이력서 DB의 이름과 스킬을 가져와서 출력
+	
+	@Insert("insert into mark_fre_db(user_id, mark_fre_id) values(#{user_id}, #{mark_fre_id})")
+	public int registerMarkFree(MarkFreeVO vo);
 	
 }
